9d54079a610a011ba91ac0e86549a86b547e25f3
paula <81623712+Paularix@users.noreply.github.com>
2022-03-14 16:25:13 +0100

    Create Lista.java

---
+++ src/model/Lista.java
@@ -0,0 +1,29 @@
+
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author paulamc5
+ * @param <T>
+ */
+public class Lista<T> {
+
+
+protected ArrayList<T> lista;
+
+
+ public void mostrar(){
+ }
+ 
+ public void añadir(){
+        
+ }
+ public void eliminar(){
+     
+     
+ }
+   
+
+}
